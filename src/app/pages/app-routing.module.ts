8183d9de752b7c454b5720829544496e5a7d6612
Vinícius Martins <viniciusmartinsg@hotmail.com>
2023-01-25 00:41:37 -0300

    redirect

--- src/app/pages/app-routing.module.ts
+++ src/app/pages/app-routing.module.ts
@@ -3,12 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -42,6 +38,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
 ];
 
